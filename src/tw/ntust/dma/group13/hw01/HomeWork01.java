6a4d6d8a01c38ef85ae02cdc2027077e582bcab9
Firman Rosdiansyah <firman.droid@gmail.com>
2013-03-31 01:58:29 +0800

    initial commit

---
+++ src/tw/ntust/dma/group13/hw01/HomeWork01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.ntust.dma.group13.hw01;
+
+/**
+ *
+ * @author firman
+ */
+public class HomeWork01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
