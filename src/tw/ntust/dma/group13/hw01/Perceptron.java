93cda63f6662da9011f64abfc944a202e48a032e
Firman Rosdiansyah <firman.droid@gmail.com>
2013-04-07 17:15:56 +0800

    Testing Perceptron

--- src/tw/ntust/dma/group13/hw01/Perceptron.java
+++ src/tw/ntust/dma/group13/hw01/Perceptron.java
@@ -1,5 +1,6 @@
 package tw.ntust.dma.group13.hw01;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 /**
@@ -17,13 +18,14 @@ public class Perceptron implements MachineLearningInterface{
      *
      * @param threshold
      */
-    public Perceptron(double threshold,double learning_rate) {
+    public Perceptron(int Ni,double threshold,double learning_rate) {
         this.threshold = 0.5;
         this.learning_rate = 0.1;
+        this.weigths = new double[Ni];
     }
 
-    public Perceptron() {
-        this(0.5,0.1);
+    public Perceptron(int Ni) {
+        this(Ni,0.5,0.1);
     }
     
     private double dot_product(double [] values){
@@ -39,17 +41,19 @@ public class Perceptron implements MachineLearningInterface{
         for(;;){
             error_count=0;
             for(double[][] data: set){
+                System.out.println(Arrays.toString( weigths));
                 double[] input_data=data[0];
                 int result = dot_product(input_data)>this.threshold?1:0;
                 double error = data[1][0] - result;
                 if (error != 0) {
                     error_count++;
                     int n = input_data.length;//ni
-                    for(int i=0;i<n;n++ ) {
+                    for(int i=0;i<n;i++ ) {
                          this.weigths[i] +=  learning_rate * error * input_data[i];
                     }
                 }
             }
+            if(error_count==0)break;
             
         }
     }
@@ -60,8 +64,26 @@ public class Perceptron implements MachineLearningInterface{
     
     public static void main(String[] args) {
         double[][][] training_set;
-        training_set = new double [][][]{{{1, 0, 0}, {1,}}, {{1, 0, 1}, {1,}}, {{1, 1, 0}, {1}}, {{1, 1, 1}, {0}}};
-        Perceptron p = new Perceptron(); 
+        //training_set = new double [][][]{{{1, 0, 0}, {1,}}, {{1, 0, 1}, {1,}}, {{1, 1, 0}, {1}}, {{1, 1, 1}, {0}}};
+        /* //AND
+        training_set = new double [][][] {
+            {{0,0},{0}},
+            {{0,1},{0}},
+            {{1,0},{0}},
+            {{1,1},{1}},
+            
+        };
+        */
+        /* //OR */
+        training_set = new double [][][] {
+            {{0,0},{0}},
+            {{0,1},{1}},
+            {{1,0},{1}},
+            {{1,1},{1}},
+            
+        };
+        //*/
+        Perceptron p = new Perceptron(2); 
         
         p.Train(training_set);
         
